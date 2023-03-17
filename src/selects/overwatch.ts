841e52a24f3b5dab99465662708244b567a1af26
Lucas Gonçalves <ldgoncalvs@gmail.com>
2023-03-17 07:43:40 -0300

    chore: updating interaction so when user finishes those questions a new channel will be created to handle imgur API

--- src/selects/overwatch.ts
+++ src/selects/overwatch.ts
@@ -4,9 +4,38 @@ import {
     ActionRowBuilder,
     StringSelectMenuBuilder,
     Message,
+    ButtonBuilder,
+    ButtonStyle,
+    TextInputBuilder,
+    TextInputStyle,
 } from 'discord.js';
 
 const selectedRolesAndRanks = [];
+
+function createCollector(
+    message: Message<boolean>,
+    customId: string,
+    nextStep?: (interaction: any) => any
+) {
+    const filter = (interaction: { isSelectMenu: () => any; customId: string }) =>
+        interaction.isSelectMenu() && interaction.customId === `${customId}`;
+    const collector = message.createMessageComponentCollector({
+        filter,
+        time: 60000,
+    });
+
+    collector.on('collect', async (interaction) => {
+        if (interaction.isStringSelectMenu()) {
+            nextStep(interaction);
+        }
+    });
+
+    collector.on('end', (collected, reason) => {
+        selectedRolesAndRanks.length = 0;
+
+        console.log(`Collector ended for reason: ${reason}`);
+    });
+}
 
 export async function handleOverwatchRole(
     interaction: AnySelectMenuInteraction<CacheType>
@@ -125,7 +154,7 @@ export async function handleSelectMenus(
                 // reached the end of roles, do something with the final values here
             } else {
                 selectedRolesAndRanks.push(`${currentRole} - ${interaction.values[0]}`);
-                // console.log(selectedRolesAndRanks);
+
                 await handleSelectMenus(interaction, values, nextRoleIndex, options);
             }
         }
@@ -135,81 +164,170 @@ export async function handleSelectMenus(
 export async function setNumberOfRank(
     interaction: AnySelectMenuInteraction<CacheType>,
     allRoles: string[],
-    currentRole?: string | null
+    currentRoleIndex?: number | null
 ) {
+    if (currentRoleIndex !== allRoles.length) {
+        const options = [
+            {
+                label: '1',
+                value: '1',
+            },
+            {
+                label: '2',
+                value: '2',
+            },
+            {
+                label: '3',
+                value: '3',
+            },
+            {
+                label: '4',
+                value: '4',
+            },
+            {
+                label: '5',
+                value: '5',
+            },
+        ];
+
+        const selectMenu: any = new ActionRowBuilder().setComponents(
+            new StringSelectMenuBuilder()
+                .setCustomId(
+                    currentRoleIndex
+                        ? `$role_options_ow2_allRoles${currentRoleIndex}`
+                        : 'role_options_ow2_allRoles'
+                )
+                .setOptions(options)
+                .setPlaceholder('Nenhuma Número de Rank Selecionado')
+        );
+
+        const message = await interaction.update({
+            fetchReply: true,
+            content: `Conta: Overwatch 2 | ${allRoles} | Selecione o Número do Rank de ${
+                allRoles[currentRoleIndex ? currentRoleIndex : 0].split('-')[0]
+            }:`,
+            components: [selectMenu],
+        });
+
+        createCollector(
+            message,
+            currentRoleIndex
+                ? `$role_options_ow2_allRoles${currentRoleIndex}`
+                : 'role_options_ow2_allRoles',
+            (interaction) => {
+                const rankToSet = currentRoleIndex
+                    ? selectedRolesAndRanks[currentRoleIndex]
+                    : selectedRolesAndRanks[0];
+
+                const index = selectedRolesAndRanks.indexOf(rankToSet);
+
+                selectedRolesAndRanks[index] = `${rankToSet} ${interaction.values[0]}`;
+
+                console.log(
+                    selectedRolesAndRanks,
+                    rankToSet,
+                    interaction.values[0],
+                    index
+                );
+                setNumberOfRank(
+                    interaction,
+                    allRoles,
+                    currentRoleIndex ? currentRoleIndex + 1 : 1
+                );
+            }
+        );
+    } else {
+        selectPlatform(interaction);
+    }
+}
+
+async function selectPlatform(interaction: any) {
     const options = [
         {
-            label: '1',
-            value: '1',
+            label: 'Xbox',
+            value: 'Xbox',
         },
         {
-            label: '2',
-            value: '2',
+            label: 'PC',
+            value: 'PC',
         },
         {
-            label: '3',
-            value: '3',
-        },
-        {
-            label: '4',
-            value: '4',
-        },
-        {
-            label: '5',
-            value: '5',
+            label: 'PSN',
+            value: 'PSN',
         },
     ];
 
     const selectMenu: any = new ActionRowBuilder().setComponents(
         new StringSelectMenuBuilder()
-            .setCustomId('role_options_ow2_allRoles')
+            .setCustomId('platform')
             .setOptions(options)
-            .setPlaceholder('Nenhuma Número de Rank Selecionado')
+            .setPlaceholder('Nenhuma Plataforma Selecionada')
     );
-
-    if (currentRole) {
-        const message = await interaction.update({
-            fetchReply: true,
-            content: `Conta: Overwatch 2 | ${allRoles} | Selecione o Número do Rank de ${
-                allRoles[0].split('-')[0]
-            }:`,
-            components: [selectMenu],
-        });
-
-        createCollector(message, 'role_options_ow2_allRoles', () =>
-            setNumberOfRank(interaction, allRoles)
-        );
-    }
 
     const message = await interaction.update({
         fetchReply: true,
-        content: `Conta: Overwatch 2 | ${allRoles} | Selecione o Número do Rank de ${
-            allRoles[0].split('-')[0]
-        }:`,
+        content: 'Conta: Overwatch 2 | Selecione sua Plataforma:',
         components: [selectMenu],
     });
 
-    // console.log(interaction, role );
+    createCollector(message, 'platform', (interaction) => {
+        hasBattletagChange(interaction);
+    });
 }
 
-//     const message = await interaction.update({ fetchReply: true, content: `Conta: Overwatch 2 - ${values} - Selecione o Rank de ${currentRole}:`, components: [menusMap.get(`role_options_ow2_${currentRole}`)] });
-// }
+async function hasBattletagChange(interaction) {
+    const buttonNo = new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+            .setCustomId('no_battletag')
+            .setEmoji('❌')
+            .setLabel('Não')
+            .setStyle(ButtonStyle.Secondary)
+    );
 
-function createCollector(
-    message: Message<boolean>,
-    customId: string,
-    nextStep?: () => any
-) {
+    const buttonYes = new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+            .setCustomId('yes_battletag')
+            .setEmoji('✅')
+            .setLabel('Sim')
+            .setStyle(ButtonStyle.Primary)
+    );
+
+    const input = new TextInputBuilder()
+        .setCustomId('competitive_points_ow2')
+        .setLabel('Quantos Pontos Competitivos tem na Conta?')
+        .setRequired(true)
+        .setStyle(TextInputStyle.Short)
+        .setPlaceholder('Insira a quantidade (números apenas)');
+
+    const response = await interaction.update({
+        fetchReply: true,
+        content:
+      'Conta: Overwatch 2 | Sua Conta tem mudança de nome (battletag) grátis?',
+        components: [buttonYes, buttonNo],
+        embeds: [],
+    });
+
     const filter = (interaction: { isSelectMenu: () => any; customId: string }) =>
-        interaction.isSelectMenu() && interaction.customId === `${customId}`;
-    const collector = message.createMessageComponentCollector({
+        interaction.customId === 'yes_battletag' || interaction.customId === 'no_battletag' || interaction.customId === 'competitive_points_ow2';
+    const collector = response.createMessageComponentCollector({
         filter,
         time: 60000,
     });
 
     collector.on('collect', async (interaction) => {
-        if (interaction.isStringSelectMenu()) {
-            return nextStep();
-        }
+
+        interaction.update({  content:
+            'Vamos prosseguir com outras perguntas :)', components: []});
+
     });
+
+    // createCollector(response, 'yes_battletag', (interaction) => {
+
+    //     console.log(interaction);
+    //     // hasBattletagChange(interaction);
+    // });
+
+    // createCollector(response, 'no_battletag', (interaction) => {
+    //     // hasBattletagChange(interaction);
+    // });
 }
